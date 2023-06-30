ff388c3cf85b33f72e13df2e8494dcaf5425a885
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-06-30 18:32:39 +0100

    added HomePage and removed HelloWorld Component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,122 @@
 <template>
-  <div class="home"></div>
+  <div class="home container">
+    <!-- Header -->
+    <div class="header flex">
+      <div class="left flex flex-column">
+        <h1>Invoices</h1>
+        <span>There are 7 total invoices</span>
+      </div>
+      <div class="right flex">
+        <div class="filter flex">
+          <span>Filter by status</span>
+          <img src="@/assets/icon-arrow-down.svg" alt="" />
+          <ul v-show="filterMenu" class="filter-menu">
+            <li>Draft</li>
+            <li>Pending</li>
+            <li>Paid</li>
+            <li>Clear Filter</li>
+          </ul>
+        </div>
+        <div class="button flex">
+          <div class="inner-button flex">
+            <img src="@/assets/icon-plus.svg" alt="" />
+          </div>
+          <span>New Invoice</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent, ref } from "vue";
+export default defineComponent({
   name: "Home",
   components: {},
-};
+  setup() {
+    const filterMenu = ref(false);
+    return {
+      filterMenu,
+    };
+  },
+});
 </script>
+
+<style lang="scss" scoped>
+.home {
+  color: #fff;
+
+  .header {
+    margin-bottom: 65px;
+
+    .left,
+    .right {
+      flex: 1;
+    }
+
+    .right {
+      justify-content: flex-end;
+      align-items: center;
+
+      .button,
+      .filter {
+        align-items: center;
+
+        span {
+          font-size: 12px;
+        }
+      }
+      .filter {
+        margin-right: 40px;
+        position: relative;
+
+        span,
+        img {
+          pointer-events: none;
+        }
+        img {
+          margin-left: 8px;
+          width: 9px;
+          height: 5px;
+        }
+        .filter-menu {
+          position: absolute;
+          top: 25px;
+          left: 0;
+          width: 120px;
+          list-style: none;
+          background-color: #1e2139;
+          box-shadow: 0px 4px 6px -1px rgba(0, 0, 0, 0.1),
+            0px 2px 4px -1px rgba(0, 0, 0, 0.06);
+          li {
+            padding: 10px 20px;
+            cursor: pointer;
+            font-size: 12px;
+            &:hover {
+              background-color: #7c5dfa;
+            }
+          }
+        }
+      }
+      .button {
+        cursor: pointer;
+        padding: 8px 10px;
+        background-color: #7c5dfa;
+        border-radius: 40px;
+        .inner-button {
+          margin-right: 8px;
+          border-radius: 50%;
+          padding: 8px;
+          justify-content: center;
+          align-items: center;
+          background-color: #fff;
+          img {
+            width: 10px;
+            height: 10px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
